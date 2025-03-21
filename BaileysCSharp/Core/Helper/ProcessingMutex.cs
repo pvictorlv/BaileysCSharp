a9ac8f890746dfbe39afc0e9a0e88a3cb54a52af
Juan Andrade <andrade.juan@outlook.com.br>
2025-03-21 15:59:22 -0300

    Add dispose method to ProcessingMutex

--- BaileysCSharp/Core/Helper/ProcessingMutex.cs
+++ BaileysCSharp/Core/Helper/ProcessingMutex.cs
@@ -1,11 +1,15 @@
 ﻿namespace BaileysCSharp.Core.Helper
 {
-    public class ProcessingMutex
+    public class ProcessingMutex : IDisposable
     {
+        public void Dispose()
+        {
+            semaphoreSlim.Dispose();
+        }
+
         SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
         public ProcessingMutex()
         {
-
         }
 
         public async Task Mutex(Action action)
